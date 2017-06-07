0d679b2e4af51631218affa1b7e8853f11dc1882
Kash Goudarzi <kashgoudarzi@gmail.com>
2017-06-06 21:34:17 -0700

    added node_modules

--- client/components/layout/About.js
+++ client/components/layout/About.js
@@ -10,18 +10,7 @@ export default class About extends React.Component {
 			<div className="about">
 				<div>
 					<h1>A little about Numerge</h1>
-<<<<<<< Updated upstream
 					<p>It's a cool app developed during Code Day 2017 in Seattle.</p>
-				</div>
-				<div>
-					<h1>A little <em>more</em> about Numerge</h1>
-					<p>Wouldn't it be nice if you could see your classmates' notes? Or better yet, the worlds'?
-						Numerge is a place where a
-						community of note-takers can come together and share their notes.
-						Of course, you can set your notes to private, but we encourage you share your notes with the world.</p>
-					<p>Notes are searchable by their title and tags.</p>
-=======
-					<p>It's a cool app designed during code day 2017 in Seattle Washington.</p>
 				</div>
 				<div>
 					<h1>A little <em>more</em> about Numerge</h1>
@@ -33,7 +22,6 @@ export default class About extends React.Component {
 					<p>Notes are categorized using tagging. For example, a note tagged as
 						#school/biology/anatomy would show up any time someone searches
 						for school, biology, or anatomy.</p>
->>>>>>> Stashed changes
 					<p>Created using Javascript, love, and a whole lot of caffeine at
 						<a href="https://codeday.org">Code Day</a>. By Kash Goudarzi and
 						Gifton Okoronkwo.</p>
--- server/popular.js
+++ server/popular.js
@@ -1,32 +1,8 @@
-<<<<<<< Updated upstream
-let showPopular = function () {
-	app.post('/showPopular', function (req, res) {
-		sql = `SELECT id, title, text, tags, timecreated, uuid FROM notes DESC LIMIT = 10`
-		connection.query(sql, function (err, rows, fields) {
-			for (let s in rows) {
-				let i = rows[s];
-				let temp = {
-					title: i['title'],
-					id: i['id'],
-					text: i['text'],
-					tags: i['tags'],
-					uuid: i['uuid'],
-					timecreated: i['timecreated']
-				}
-				output.push(temp);
-			}
-			res.json(output);
-		})
-	})
-}
-
-module.exports = {
-	showPopular: showPopular()
-=======
 let showPopular = function(){
     app.post('/showPopular', function(req, res){
-        sql = `SELECT id, title, text, tags, timecreated, uuid FROM notes DESC LIMIT = 10`
-        connection.query(sql, function(req, res){
+    	let output = []
+        let sql = `SELECT id, title, text, tags, timecreated, uuid FROM notes DESC LIMIT = 10`
+        connection.query(sql, function(err, rows, fields){
             for (let s in rows) {
                 let i = rows[s];
                 let temp = {
@@ -42,5 +18,4 @@ let showPopular = function(){
             res.json(output);
         })
     })
->>>>>>> Stashed changes
 }
